2e874bdbdf97000aaed6d0851788cc65026ce25f
ahmatg <66846760+ahmatg@users.noreply.github.com>
2020-10-20 22:31:28 +0500

    ahmatgaliev

---
+++ dahmatgaliev/lab1.1/lab1.1/lab1.1.cpp
@@ -0,0 +1,15 @@
+﻿///// 1
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double a, b, c, d, s;
+	cout << "Введите значения а,b,c,d\n";
+	cin >> a >> b >> c >> d;
+	s = (a / c) * (b / d) - (a * b - c) / (c * d);
+	cout << "Ответ : "<< s;
+}
+
+
